4ab861a71bae1df623ff111a135ac002f430a3dd
Leandro Lourenço <105615180+leolourencof@users.noreply.github.com>
2023-12-08 20:18:10 -0300

    feat: updated app

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+  
   await app.listen(3001);
 }
 bootstrap();
